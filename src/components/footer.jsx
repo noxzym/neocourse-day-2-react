384266a3655e6a8b5d1276fd0d65953bff516bc5
noxzym <orchitiadi@gmail.com>
2023-10-18 06:08:13 +0700

    chore(footer): update ui

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 function Footer() {
-  return (
-    <footer className="absolute bottom-0 w-full bg-white py-4">
-      <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} LokerIn</p>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="w-full border-t-2 py-4">
+            <div className="container mx-auto text-center">
+                <p className="dark:text-white">
+                    &copy; {new Date().getFullYear()} LokerIn
+                </p>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
