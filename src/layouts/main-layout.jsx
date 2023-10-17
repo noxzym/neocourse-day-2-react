ade78b44a4971e10eae935819b50d62eb640ece3
noxzym <orchitiadi@gmail.com>
2023-10-18 06:05:34 +0700

    chore(layout): add dark class

--- src/layouts/main-layout.jsx
+++ src/layouts/main-layout.jsx
@@ -2,13 +2,15 @@ import Header from "../components/Header";
 import Footer from "../components/footer";
 
 const MainLayout = ({ children }) => {
-  return (
-    <section className="relative min-h-screen bg-slate-50 backdrop-blur">
-      <Header />
-      {children}
-      <Footer />
-    </section>
-  );
+    return (
+        <section className="relative min-h-screen bg-slate-50 backdrop-blur dark:bg-slate-900">
+            <Header />
+            <main className="min-h-screen">
+                {children}
+            </main>
+            <Footer />
+        </section>
+    );
 };
 
 export default MainLayout;
